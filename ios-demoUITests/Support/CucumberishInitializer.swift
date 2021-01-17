4acf4ff22106fa20e8f62a4e967d6d98e31f194f
Nabil Shaikh <nabil.shaikh@aptushealth.com>
2021-01-17 20:34:08 +0530

    initialized 2nd test case

--- ios-demoUITests/Support/CucumberishInitializer.swift
+++ ios-demoUITests/Support/CucumberishInitializer.swift
@@ -14,10 +14,10 @@ class CucumberishInitializer: NSObject {
         
         beforeStart { () -> Void in
             TC01_LandingPage().TC01_LandingPageImplementation()
+            TC02_TabBar().TC02_TabBarImplementation()
         }
     
     let bundle = Bundle(for: CucumberishInitializer.self)
         Cucumberish.executeFeatures(inDirectory: "Features", from: bundle, includeTags: nil, excludeTags: nil)
-        
     }
 }
